cda402057e41ede259aa54036c8d2f9e7a7afd21
rahul9999 <36025572+rahul9999xda@users.noreply.github.com>
2018-05-21 16:24:49 +0530

    DotOs bringup (#4)
    * Dot bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_osprey-eng
-add_lunch_combo lineage_osprey-user
-add_lunch_combo lineage_osprey-userdebug
+add_lunch_combo dot_osprey-eng
+add_lunch_combo dot_osprey-user
+add_lunch_combo dot_osprey-userdebug
