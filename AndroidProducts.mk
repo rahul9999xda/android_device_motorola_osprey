9905f1e5879851471ac13dd3268d934474fa1117
Kyle Harrison <khwebmail@gmail.com>
2018-08-11 13:47:03 +0100

    osprey: Consider lineage_osprey product via the AOSP way

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2015 The Android Open Source Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,4 +14,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage.mk
+    $(LOCAL_DIR)/lineage_osprey.mk
+
